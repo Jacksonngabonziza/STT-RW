18c58e819371dea10290f50cbd804cc1d5bc498b
n-jackson <jackson@gennxttechsol.com>
2022-09-09 23:47:27 +0200

    bg-fx

--- main.py
+++ main.py
@@ -11,30 +11,6 @@ async def read_root():
 
     return ("Welcome to kinyarwanda Speech To Text Model API")
 
-
-@app.post("/files/")
-async def create_file(file: bytes = File()):
-    asr_model = nemo_asr.models.EncDecRNNTBPEModel.from_pretrained(
-        model_name="stt_rw_conformer_transducer_large")
-    files = [file.file()]
-    for fname, transcription in zip(files, asr_model.transcribe(paths2audio_files=files)):
-        print(f"Audio in {fname} was recognized as: {transcription}")
-        print(transcription[0])
-    return {"text": transcription[0], "file_size": len(file)}
-
-
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    asr_model = nemo_asr.models.EncDecRNNTBPEModel.from_pretrained(
-        model_name="stt_rw_conformer_transducer_large")
-    files = [file.file]
-    print("file loaded is **************",file.file)
-    for fname, transcription in zip(files, asr_model.transcribe(paths2audio_files=files)):
-        print(f"Audio in {fname} was recognized as: {transcription}")
-        print(transcription[0])
-    return {"text": transcription[0], "file_size": len(file), "filename": file.filename}
-
-
 @app.post("/transcribe/", response_description="", response_model = "")
 async def result(file:UploadFile = File(...)):
      try:
--- test.py
+++ test.py
@@ -1,24 +1,24 @@
-from fastapi  import FastAPI, UploadFile, File, status
-from fastapi.responses import JSONResponse
+# from fastapi  import FastAPI, UploadFile, File, status
+# from fastapi.responses import JSONResponse
 
-import aiofiles
-app = FastAPI( debug = True ) 
+# import aiofiles
+# app = FastAPI( debug = True ) 
 
-@app.post("/upload_file/", response_description="", response_model = "")
-async def result(file:UploadFile = File(...)):
-     try:
-        async with aiofiles.open(file.filename, 'wb') as out_file:
-            content = await file.read()  # async read
-            await out_file.write(content)  # async write
-            print(out_file.name)
+# @app.post("/upload_file/", response_description="", response_model = "")
+# async def result(file:UploadFile = File(...)):
+#      try:
+#         async with aiofiles.open(file.filename, 'wb') as out_file:
+#             content = await file.read()  # async read
+#             await out_file.write(content)  # async write
+#             print(out_file.name)
 
-     except Exception as e:
-        return JSONResponse(
-            status_code = status.HTTP_400_BAD_REQUEST,
-            content = { 'message' : str(e) }
-            )
-     else:
-        return JSONResponse(
-            status_code = status.HTTP_200_OK,
-            content = {"result":'success'}
-            )
+#      except Exception as e:
+#         return JSONResponse(
+#             status_code = status.HTTP_400_BAD_REQUEST,
+#             content = { 'message' : str(e) }
+#             )
+#      else:
+#         return JSONResponse(
+#             status_code = status.HTTP_200_OK,
+#             content = {"result":'success'}
+#             )
