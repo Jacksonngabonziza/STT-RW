37cad658305a10d408d86be561f4676657769b08
n-jackson <jackson@gennxttechsol.com>
2022-09-09 19:06:25 +0200

    init

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, File, UploadFile
+import nemo
+import nemo.collections.asr as nemo_asr
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+
+    return ("Welcome to kinyarwanda Speech To Text Model API")
+
+
+@app.post("/files/")
+async def create_file(file: bytes = File()):
+    asr_model = nemo_asr.models.EncDecRNNTBPEModel.from_pretrained(
+        model_name="stt_rw_conformer_transducer_large")
+    files = [file]
+    for fname, transcription in zip(files, asr_model.transcribe(paths2audio_files=files)):
+        print(f"Audio in {fname} was recognized as: {transcription}")
+        print(transcription[0])
+    return {"text": transcription[0], "file_size": len(file)}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    asr_model = nemo_asr.models.EncDecRNNTBPEModel.from_pretrained(
+        model_name="stt_rw_conformer_transducer_large")
+    files = [file]
+    for fname, transcription in zip(files, asr_model.transcribe(paths2audio_files=files)):
+        print(f"Audio in {fname} was recognized as: {transcription}")
+        print(transcription[0])
+    return {"text": transcription[0], "file_size": len(file), "filename": file.filename}
