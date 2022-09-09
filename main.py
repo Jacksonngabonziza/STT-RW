cf99fb49ecfb7883e50b4d1902a24063f06469b8
n-jackson <jackson@gennxttechsol.com>
2022-09-09 19:34:42 +0200

    bg-fx

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ async def read_root():
 async def create_file(file: bytes = File()):
     asr_model = nemo_asr.models.EncDecRNNTBPEModel.from_pretrained(
         model_name="stt_rw_conformer_transducer_large")
-    files = [file]
+    files = [file.file]
     for fname, transcription in zip(files, asr_model.transcribe(paths2audio_files=files)):
         print(f"Audio in {fname} was recognized as: {transcription}")
         print(transcription[0])
@@ -25,7 +25,7 @@ async def create_file(file: bytes = File()):
 async def create_upload_file(file: UploadFile):
     asr_model = nemo_asr.models.EncDecRNNTBPEModel.from_pretrained(
         model_name="stt_rw_conformer_transducer_large")
-    files = [file]
+    files = [file.file]
     for fname, transcription in zip(files, asr_model.transcribe(paths2audio_files=files)):
         print(f"Audio in {fname} was recognized as: {transcription}")
         print(transcription[0])
