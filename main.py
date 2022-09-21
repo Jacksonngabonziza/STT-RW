de6667a24bc1588ed791cc4dd96c7c24f6121e10
jackson <62749711+Jacksonngabonziza@users.noreply.github.com>
2022-09-21 11:16:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,36 @@
 from fastapi import FastAPI, UploadFile, File, status
 import aiofiles
 import nemo
-import torch
-import torchaudio
-# import tensorflow as tf
 import nemo.collections.asr as nemo_asr
 from fastapi.responses import JSONResponse
+import torchaudio
 app = FastAPI()
-
-resampler = torchaudio.transforms.Resample(48000, 16000)
-
-
-def speech_to_array(audio_path):
-    speech_array, sampling_rate = torchaudio.load(audio_path)
-    audio_length = torch.tensor([speech_array.shape[1]/sampling_rate])
-    audio = resampler(speech_array)
-    return audio, audio_length
-
-
-def tensor_to_audio(output_signal):
-    output_filename = tf.placeholder(tf.string, shape=[])
-    encoded_audio_data = tf.contrib.ffmpeg.encode_audio(
-        output_signal, file_format="wav", samples_per_second=44100)
-    write_file_op = tf.write_file(output_filename, encoded_audio_data)
 
 
 @app.get("/")
 async def read_root():
 
     return ("Welcome to kinyarwanda Speech To Text Model API")
+asr_model = nemo_asr.models.EncDecRNNTBPEModel.from_pretrained(
+                model_name="stt_rw_conformer_transducer_large")
 
-
-@app.post("/transcribe/", response_description="", response_model="")
-async def result(file: UploadFile = File(...)):
+async def resampler(audio_path):
+    resampler = torchaudio.transforms.Resample(48000, 16000) # 
+    speech_array, sampling_rate = torchaudio.load(audio_path)
+    audio = resampler(speech_array)
+    torchaudio.save(audio_path,audio,16000) # 16000 ni sampling rate
+        
+@app.post("/transcribe/", response_description="", response_model = "")
+async def result(file:UploadFile = File(...)):
      try:
-        async with aiofiles.open(file.filename, 'wb') as out_file:
+         async with aiofiles.open(file.filename, 'wb') as out_file:
             content = await file.read()  # async read
             await out_file.write(content)  # async write
-            print("########## saving locall done")
-            cleaned_audio = speech_to_array(out_file.name)
-            # out_audio = tf.audio.encode_wav(cleaned_audio, 44100, name=None)
-            # print("Type of out audio is :", type(out_audio))
-            print("type of cleaned object: ",type(cleaned_audio))
-            print("########## cleaning done")
-
-            # async with aiofiles.open(cleaned_audio, 'wb') as out_file:
-            #     conte = await cleaned_audio.read()  # async read
-            #     print("########## content reading from clean  done")
-            #     await out_file.write(conte)
-            #     print("########## saving cleaned  done")
-
             print(out_file.name)
-            asr_model=nemo_asr.models.EncDecRNNTBPEModel.from_pretrained(
-            model_name="stt_rw_conformer_transducer_large")
-            # speech_to_array(out_file.name)
-            files=[out_file.name]
-            print("file loaded is **************", out_file.name)
+            resampler(out_file.name)
+            files = [out_file.name]
+            
+            # print("file loaded is **************",file.file)
             for fname, transcription in zip(files, asr_model.transcribe(paths2audio_files=files)):
                 print(f"Audio in {fname} was recognized as: {transcription}")
                 print(transcription[0])
@@ -65,11 +40,9 @@ async def result(file: UploadFile = File(...)):
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
             content={'message': str(e)}
-            )
+        )
      else:
         return JSONResponse(
             status_code=status.HTTP_200_OK,
             content={"result": transcription[0]}
-            )
-
-# output_signal = ...  # A 2-D tensor, [samples x channels]
+        )
