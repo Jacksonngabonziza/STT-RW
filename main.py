a6666c871e043ca9df5f68b73c358b0b36732ac5
jackson <62749711+Jacksonngabonziza@users.noreply.github.com>
2022-09-22 08:51:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ async def result(file:UploadFile = File(...)):
                 sound = AudioSegment.from_mp3(out_file.name)
                 sound.export(out_file.name, format="wav")
                 print("#############mp3 detected#################")
-            if file.name.endswith("wav"):
+            if fout_file.name.endswith("wav"):
                 pac.convert_wav_to_16bit_mono(out_file.name,out_file.name)
             files = [out_file.name]
             speech_array, sampling_rate = torchaudio.load(out_file.name)
