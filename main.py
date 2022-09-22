cd0a8ae85f9bfb0aa1a6ab85d676fe2a13f80452
jackson <62749711+Jacksonngabonziza@users.noreply.github.com>
2022-09-22 08:00:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -38,8 +38,8 @@ async def result(file:UploadFile = File(...)):
             content = await file.read()  # async read
             await out_file.write(content)  # async write
             print(out_file.name)
-            resampler(out_file.name)
-#             resample_ffmpg(out_file.name)
+#             resampler(out_file.name)
+            resample_ffmpg(out_file.name)
 #             pac.convert_wav_to_16bit_mono(out_file.name,out_file.name)
 #             if not out_file.name.endswith("mp3"):
 #                 sound = AudioSegment.from_mp3(out_file.name)
