5a3326d0e8acc4815d1792d5c28c44a414414216
jackson <62749711+Jacksonngabonziza@users.noreply.github.com>
2022-09-22 08:26:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -40,12 +40,13 @@ async def result(file:UploadFile = File(...)):
             await out_file.write(content)  # async write
             print(out_file.name)
 #             resampler(out_file.name)
-            resample_ffmpg(out_file.name)
+#             resample_ffmpg(out_file.name)
 #             pac.convert_wav_to_16bit_mono(out_file.name,out_file.name)
-#             if not out_file.name.endswith("mp3"):
-#                 sound = AudioSegment.from_mp3(out_file.name)
-#                 sound.export(out_file.name, format="wav")
-#             pac.convert_wav_to_16bit_mono(out_file.name,out_file.name)
+            if not out_file.name.endswith("mp3"):
+                sound = AudioSegment.from_mp3(out_file.name)
+                sound.export(out_file.name, format="wav")
+                print("mp3 detected")
+            pac.convert_wav_to_16bit_mono(out_file.name,out_file.name)
             files = [out_file.name]
             speech_array, sampling_rate = torchaudio.load(out_file.name)
             print("updated sample rate is:",sampling_rate)
