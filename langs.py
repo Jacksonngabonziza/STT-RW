94d6fb31d47d1a841a147468a8f52cf95804ad82
jackson <ngabojck@gmail.com>
2022-09-27 18:46:37 +0200

    init translation

---
+++ langs.py
@@ -0,0 +1,8 @@
+LANGS = [
+    "eng_Latn",
+    "fra_Latn",
+    "kin_Latn",
+    "lin_Latn",
+    "lug_Latn",
+    "swh_Latn",
+]
---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI,status
+from fastapi.responses import JSONResponse
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+import torch
+# from ui import title, description, examples
+from langs import LANGS
+app = FastAPI()
+TASK = "translation"
+CKPT = "facebook/nllb-200-distilled-600M"
+model = AutoModelForSeq2SeqLM.from_pretrained(CKPT)
+tokenizer = AutoTokenizer.from_pretrained(CKPT)
+device = 0 if torch.cuda.is_available() else -1
+
+@app.get("/")
+async def read_root():
+
+    return ("Welcome to machine translation  we can help you translate 5 languages ")
+
+@app.post("/translate/", response_description="", response_model = "")
+async def result(text, src_lang, tgt_lang, max_length=400):
+    translation_pipeline = pipeline(TASK,
+                                    model=model,
+                                    tokenizer=tokenizer,
+                                    src_lang=src_lang,
+                                    tgt_lang=tgt_lang,
+                                    max_length=max_length,
+                                    device=device)
+    result = translation_pipeline(text)
+    return result[0]['translation_text']
