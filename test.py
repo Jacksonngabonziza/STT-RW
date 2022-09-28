f6c1848c12ece3f215f2ad507cd11e6c20bc13c1
jackson <62749711+Jacksonngabonziza@users.noreply.github.com>
2022-09-28 11:46:25 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
-print("TensorFlow version:", tf.__version__)
-print("is_gpu_available:", tf.test.is_gpu_available())
+print("TensorFlow version############:", tf.__version__)
+print("is_gpu_available########:", tf.test.is_gpu_available())
 import tensorflow.compiler as tf_cc
-print("is TensorRT enabled:", tf_cc.tf2tensorrt._pywrap_py_utils.is_tensorrt_enabled())
-print("loaded trt ver:", tf_cc.tf2tensorrt._pywrap_py_utils.get_loaded_tensorrt_version())
-print("linked trt ver:", tf_cc.tf2tensorrt._pywrap_py_utils.get_linked_tensorrt_version())
+print("is TensorRT enabled:######", tf_cc.tf2tensorrt._pywrap_py_utils.is_tensorrt_enabled())
+print("loaded trt ver:########", tf_cc.tf2tensorrt._pywrap_py_utils.get_loaded_tensorrt_version())
+print("linked trt ver:#########", tf_cc.tf2tensorrt._pywrap_py_utils.get_linked_tensorrt_version())
