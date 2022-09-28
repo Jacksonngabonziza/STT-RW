9b3052f259d05292a9af5d5edc16d74f56c55073
jackson <62749711+Jacksonngabonziza@users.noreply.github.com>
2022-09-28 10:50:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ import torch
 # from ui import title, description, examples
 from langs import LANGS
 app = FastAPI()
+
+# device ="cpu"
+# device=device
+@app.get("/")
+async def read_root():
+    return ("Welcome to machine translation  we can help you translate 5 languages ")
+
+@app.post("/translate/", response_description="", response_model = "")
 TASK = "translation"
 CKPT = "facebook/nllb-200-distilled-600M"
 model = AutoModelForSeq2SeqLM.from_pretrained(CKPT).cpu()
 # to('cpu')
 tokenizer = AutoTokenizer.from_pretrained(CKPT)
-device ="cpu"
-
-@app.get("/")
-async def read_root():
-
-    return ("Welcome to machine translation  we can help you translate 5 languages ")
-
-@app.post("/translate/", response_description="", response_model = "")
 async def result(text, src_lang, tgt_lang, max_length=400):
     translation_pipeline = pipeline(TASK,
                                     model=model,
@@ -25,6 +25,6 @@ async def result(text, src_lang, tgt_lang, max_length=400):
                                     src_lang=src_lang,
                                     tgt_lang=tgt_lang,
                                     max_length=max_length,
-                                    device=device)
+                                    )
     result = translation_pipeline(text)
     return result[0]['translation_text']
