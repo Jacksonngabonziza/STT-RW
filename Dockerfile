bf0db5ac13ed5332d8649373c6c25fc24972e98e
Samuel Rutunda <samuel@digitalumuganda.com>
2023-02-11 11:41:18 +0200

    deploy to server

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM tiangolo/uvicorn-gunicorn:python3.8
+
+RUN apt-get update
+RUN apt-get install -y libsndfile1 espeak-ng git
+RUN pip install --upgrade pip
+
+WORKDIR /code/tts-api
+COPY ./app/ /code/tts-api
+COPY requirements.txt /code/asr-api/
+RUN pip install -r requirements.txt
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
---
+++ app/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, UploadFile, File, status
+from fastapi.responses import StreamingResponse,JSONResponse
+
+import tempfile
+from typing import Optional
+import numpy as np
+from TTS.utils.synthesizer import Synthesizer
+
+app = FastAPI()
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',datefmt='%d-%b-%y %H:%M:%S',level=logging.INFO)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+    allow_origins=['*'])
+
+@app.get("/")
+async def read_root():
+    return ("Welcome to kinyarwanda Text To Text Model API")
+MAX_TXT_LEN=3000
+
+@app.post("/generate_audio/", response_description="", response_model = "")
+async def generate_audio(text: str):
+    if len(text) > MAX_TXT_LEN:
+        text = text[:MAX_TXT_LEN]
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={'message': f"Error:Input text was cutoff since it went over the {MAX_TXT_LEN} character limit."}
+        )
+    synthesizer = Synthesizer("./Kinyarwanda_YourTTS/model.pth",
+            "Kinyarwanda_YourTTS/config.json",
+            tts_speakers_file="Kinyarwanda_YourTTS/speakers.pth",
+            encoder_checkpoint="Kinyarwanda_YourTTS/SE_checkpoint.pth.tar",
+            encoder_config="Kinyarwanda_YourTTS/config_se.json",)
+    wav = synthesizer.tts(text, speaker_wav="Kinyarwanda_YourTTS/conditioning_audio.wav")
+    headers = {
+        "Content-Disposition": "attachment; filename=" + str(text[:10]) + ".wav"
+    }
+    return StreamingResponse(
+        wav, media_type="application/octet-stream", headers=headers
+    )
+    # with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as fp:
+    #     synthesizer.save_wav(wav, fp)
+    #     return fp.name
