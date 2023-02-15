77933e62918df6885d208490c51e4be6a76018b1
jackson <62749711+Jacksonngabonziza@users.noreply.github.com>
2023-02-15 03:29:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -65,8 +65,8 @@ async def create_file(file: bytes = File(...)):
             print("#################### converted successfully")
             files = [file_name]
             print("#################### file loaded successfully")
-            # speech_array, sampling_rate = torchaudio.load(file_name)
-            # print("updated sample rate is:",sampling_rate)
+            speech_array, sampling_rate = torchaudio.load(file_name)
+            print("updated sample rate is:",sampling_rate)
             # print("file loaded is **************",file.file)
             start = timeit.default_timer()
             for fname, transcription in zip(files, asr_model.transcribe(paths2audio_files=files)):
